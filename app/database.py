5c797dcdf0f94524396c8c5d600510451e9808aa
mchlbschmdt <36120562+mchlbschmdt@users.noreply.github.com>
2025-03-29 13:31:30 -0400

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base
+from .models import Base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./properties.db"  # You can replace this with your DB URI
 
