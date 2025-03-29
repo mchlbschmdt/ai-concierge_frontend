60107f5f29493d958841397af9c9d73ebdb90e0e
mchlbschmdt <36120562+mchlbschmdt@users.noreply.github.com>
2025-03-29 13:46:24 -0400

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,11 +1,17 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .models import Base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./properties.db"  # You can replace this with your DB URI
+DATABASE_URL = "mysql://root:ZgLszGvzUZiysclJEiyfSyXEruAOxGYP@turntable.proxy.rlwy.net:53890/railway"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})  # For SQLite only
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
-# Create the database tables
-Base.metadata.create_all(bind=engine)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
